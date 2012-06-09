994d84e8db66f4a5bef9b457be7b21a33a710a6f
Quentin Pleplé <quentin.pleple@gmail.com>
2012-06-09 12:40:25 -0700

    correlated topics

--- experiment.py
+++ experiment.py
@@ -59,32 +59,48 @@ def printtopics(tops, n = 20):
   
   print '\n'.join(lines)
 
+def savetopics(olda):
+  olda._Elogbeta = onlineldavb.dirichlet_expectation(olda._lambda)
+  olda._expElogbeta = n.exp(olda._Elogbeta)
+  
+
+def mergetopics(olda, k1, k2):
+  cprint('Merging both topics into %s' % k1, 'yellow')
+  olda._lambda[k1] += olda._lambda[k2] * sum(olda._lambda[k1]) / sum(olda._lambda[k2])
+  
+  cprint('Reset topic %s' % k2, 'yellow')
+  olda._lambda[k2] = 1*n.random.gamma(100., 1./100., olda._W)
+  savetopics(olda)
+
 def resettopics(olda, topics):
   for k in topics:
     cprint('Reset topic %s' % k, 'yellow')
     olda._lambda[k] = 1*n.random.gamma(100., 1./100., olda._W)
-    olda._Elogbeta = onlineldavb.dirichlet_expectation(olda._lambda)
-    olda._expElogbeta = n.exp(olda._Elogbeta)
+    savetopics(olda)
     
 if (len(sys.argv) < 2):
-  cprint(' Running with variable in memory! ', 'white', 'on_red')
+  pass
 elif sys.argv[1] == 'new':
   # Initialize the algorithm with alpha=1/K, eta=1/K, tau_0=1024, kappa=0.7
   olda = onlineldavb.OnlineLDA(vocab, K, D, 1./K, 1./K, 1024., 0.7)
   tops = []
 elif sys.argv[1] == 'load':
-  cprint('Loading model', 'yellow')
   if len(sys.argv) == 2:
-    olda = cPickle.load(open('olda.pickle'))
-    tops = cPickle.load(open('tops.pickle'))
+    cprint(' What model? ', 'white', 'on_red')
+    sys.exit()
+    # olda = cPickle.load(open('olda.pickle'))
+    # tops = cPickle.load(open('tops.pickle'))
   else:
+    cprint('Loading model %s' % sys.argv[2], 'yellow')
     olda = cPickle.load(open('%s-model.pickle' % sys.argv[2]))
     tops = cPickle.load(open('%s-tops.pickle' % sys.argv[2]))
 else:
   cprint(' Invalid argument ', 'white', 'on_red')
   sys.exit()
 
-def main(olda, tops, save = False):
+
+merging = []
+def main(olda, tops, n = 20):
   # The number of documents to analyze each iteration
   batchsize = 1024
   # The total number of documents in Wikipedia
@@ -96,25 +112,78 @@ def main(olda, tops, save = False):
   W = len(vocab)
   
   prevlambda = olda._lambda
+  lastmerge = 0
   while True:
     it = olda._updatect
     cprint('Iteration %s: download %s random articles from Wikipedia' % (it, batchsize), 'yellow')
-    # start_time_it = time.time()
     (docset, articlenames) =  wikirandom.get_random_wikipedia_articles(batchsize)
-    # cprint('%.1f seconds' % (time.time() - start_time_it), 'blue')
   
-    # start_time = time.time()
     cprint('Give them to online LDA', 'yellow')
     olda.update_lambda(docset)
-    # cprint('%.1f seconds' % (time.time() - start_time), 'blue')
     
     top = [topwords(olda._lambda[k,:], vocab, 30) for k in range(0, len(olda._lambda))]
     tops.append(top)
-    printtopics(tops)
-  
-    if save:
-      cprint('Saving model...', 'yellow')
-      if it % 10 == 0:
-        cPickle.dump(olda, open('olda.pickle', 'wb'))
-      cPickle.dump(tops, open('tops.pickle', 'wb'))
-    # cprint('%.1f seconds for the iteration' % (time.time() - start_time_it), 'blue')
+    printtopics(tops, n)
+
+    cprint('Top 3 correlated topics', 'yellow')
+    corr = topcorrelated(olda)
+    for i in range(0, 3):
+      (dot, (k1, k2)) = corr[i]
+      cprint('Correlation %s' % dot, 'green')
+      s = colored(k1, 'green')
+      s += ' ' + ' '.join(topwords(olda._lambda[k1], vocab, n))
+      print s
+      s = colored(k2, 'green')
+      s += ' ' + ' '.join(topwords(olda._lambda[k2], vocab, n))
+      print s
+    
+    
+    (dot, (k1, k2)) = corr[0]
+    if dot > 0.5 and it - lastmerge > 10:
+      cprint(' Merging topics %s and %s ' % (k1, k2), 'white', 'on_blue')
+      mergetopics(olda, k1, k2)
+      merging.append(corr[0])
+      cPickle.dump(merging, open('merging', 'wb'))
+      lastmerge = it
+
+def norms(olda, t):
+  l = olda._lambda[t]
+  values = [(n.dot(l, lk), k) for (k, lk) in enumerate(list(olda._lambda)) if k != t]
+  return sorted(values)
+
+def maxdot(olda):
+  l = olda._lambda
+  mdot = 0
+  midx = (0, 0)
+  for i in range(0, len(l) - 1):
+    for j in range(i + 1, len(l)):
+      dot = n.dot(l[i], l[j])
+      if dot > mdot:
+        mdot = dot
+        midx = (i, j)
+        print 'new max: %s for the topics %s-%s' % (colored(mdot, 'yellow'), colored(i, 'yellow'), colored(j, 'yellow'))
+  return midx
+
+def topcorrelated(olda):
+  l = olda._lambda
+  dots = []
+  for i in range(0, len(l) - 1):
+    for j in range(i + 1, len(l)):
+      dots.append((n.dot(l[i] / n.linalg.norm(l[i]), l[j] / n.linalg.norm(l[j])), (i, j)))
+  return sorted(dots, reverse = True)
+
+def propmerge(olda):
+  vocab = file('mdhoffma/dictnostops.txt').read().split()
+  corr  = topcorrelated(olda)
+  corr  = [(dot, idx) for (dot, idx) in corr if dot > 0.5]
+  for (dot, idx) in corr:
+    cprint('Correlation %s' % dot, 'yellow')
+    (k1, k2) = idx
+
+    s = colored(k1, 'green')
+    s += ' ' + ' '.join(topwords(olda._lambda[k1], vocab, 20))
+    print s
+
+    s = colored(k2, 'green')
+    s += ' ' + ' '.join(topwords(olda._lambda[k2], vocab, 20))
+    print s
